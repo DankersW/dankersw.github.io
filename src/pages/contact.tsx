2f537ef1c0aaeb4dc6d8732ac773a7a6863b8fcd
DankersW <wouter.dankers@skynet.be>
2021-03-29 21:23:42 +0200

    pages dummy structure added

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./contact.css"
+
+export default function Contact() {
+    return (
+        <div className="contact-container">
+            <h1 className="section-title">Contact</h1>
+            <h2 className="sub-title">Let's become internet BFF's</h2>
+        </div>
+    );
+}
