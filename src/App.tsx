d0d0368124f0dc391270f6e6644d950f72e2c824
DankersW <wouter.dankers@skynet.be>
2021-03-29 15:45:09 +0200

    Added footer with parallex effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import './App.css';
 import IntroPage from './pages/into'
+import Footer from './pages/footer'
 
 function App() {
   return (
-    <div className="main">
-      <IntroPage></IntroPage>
-      
-      <div className="section-a">
-        <h2>Section A</h2>
-      </div>
-
-      <div className="section-x">
-        <h2>Section X</h2>
-      </div>
-
-      <div className="footer">
-        <h4>Footer pic</h4>
-        <div className="arrow">
+    <div className="app">
+      <div className="parallax-bg"/>
+      <div className="content">
+        <IntroPage></IntroPage>
+        
+        <div className="section-a">
+          <h2>Section A</h2>
         </div>
-      </div>
 
+        <div className="section-x">
+          <h2>Section X</h2>
+        </div>
+
+        <Footer/>
+        
+      </div>
+      
+        
+
+      
     </div>
   );
 }
--- src/pages/into.tsx
+++ src/pages/into.tsx
@@ -8,7 +8,7 @@ export default function IntoPage() {
         <div className="container">
             <span/>
             <div className="flexitem-text">
-                <h1 className="into-name">Hi, I'm Wouter Dankers,</h1>
+                <h1 className="into-name">Hi, I'm Wouter Dankers</h1>
                 <h3 className="into-title">A software engineer</h3>
             </div>
             <div className="flexitem-scroll-down">
