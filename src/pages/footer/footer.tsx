72b86707e56ab5767ef48ee4a905790243fc5cc1
DankersW <wouter.dankers@skynet.be>
2021-03-30 11:24:38 +0200

    Restructered pages dir

---
+++ src/pages/footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css"
+
+export default function Footer() {
+    return (
+        <div className="footer-container bg-grey">
+            <div className="footer-content">
+                © 2021 Copyright: Wouter Dankers! All Rights Reserved
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/projects/projects.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./projects.css"
+
+export default function Projects() {
+    return (
+        <div className="projects-container section-wrapper bg-grey">
+            <h1 className="section-title">Projects</h1>
+        </div>
+    );
+}
