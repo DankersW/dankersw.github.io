5a3356a0c8096b69f9325f74d04c5b59b24e4862
DankersW <wouter.dankers@skynet.be>
2021-03-30 11:05:36 +0200

    Contact page made mobile friendly

--- src/pages/into.tsx
+++ src/pages/into.tsx
@@ -5,7 +5,7 @@ import ScrolDown from "./../components/scrol/scrolDown"
 
 export default function IntoPage() {
     return (
-        <div className="container">
+        <div className="container bg-grey">
             <span/>
             <div className="flexitem-text">
                 <h1 className="into-name">Hi, I'm Wouter Dankers</h1>
