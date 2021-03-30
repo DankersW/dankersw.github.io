1c495d72ce10b1b1a86124cad9d96c386a3b02f3
DankersW <wouter.dankers@skynet.be>
2021-03-30 16:17:41 +0200

    Basic layout about and project page

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import "./about.css"
+import wouterSnowPic from "./../../images/wouter.jpg"
 
 export default function About() {
     return (
         <div className="about-container bg-dark-grey section-wrapper">
             <h1 className="section-title">About</h1>
             <div className="about-flex-container">
-                <div className="color1 flex-item">#222831</div>
-                <div className="color2 flex-item">#30475e</div>
-                <div className="color3 flex-item">#f2a365</div>
-                <div className="color4 flex-item">#ECECEC</div>
+                <div className="about-info">
+                    Life story: 
+                    Born and raised in the country side of Belgium. This also the place I lived to longest time of my life. 
+                    I took a bachelors in ICT in Geel Belgium and during my bachelors I studied in Sweden and did an internship in Beijing China. 
+
+                    Hobbies and interests:
+
+                    Currently I'm living together with my girlfriend Mathilda in a small apartment in the center of Göteborg.
+
+
+                </div> 
+                <div className="about-pic">
+                    <img src={wouterSnowPic} alt="BigCo Inc. logo"/>
+                </div>
             </div>
         </div>
     );
--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -5,6 +5,31 @@ export default function Projects() {
     return (
         <div className="projects-container section-wrapper bg-grey">
             <h1 className="section-title">Projects</h1>
+            <h3 className="sub-sub-title">
+                A selection of some of my personal projects. All source code can be found on <a href="https://github.com/DankersW" target="_blank" rel="noopener noreferrer">Github</a>
+            </h3>
+            <div className="project-grid">
+                <div className="project-tile">
+                    <div className="project-info">
+                        <h3 className="project-title">Home automation</h3>
+                        <p>Some long text here</p>
+                        <p>Tools</p>
+                        <p>link</p>
+                    </div>
+                    <div className="project-bg">
+                        pic
+                    </div>
+                    
+                </div>
+                <div className="project-tile">Portfolio website</div>
+                <div className="project-tile">Snow plow robot</div>
+                <div className="project-tile">IEEE publicaion</div>
+                <div className="project-tile">AI poker player</div>
+                <div className="project-tile">DIY bed with storage</div>
+                <div className="project-tile">Drone</div>
+
+            </div>
+
         </div>
     );
 }
