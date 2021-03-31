5ab2c36d04ee66f06c3f5d8267c741b819a16c17
DankersW <wouter.dankers@skynet.be>
2021-03-31 12:48:58 +0200

    Project grid items idea conceptiolized

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./projects.css"
 
+import bg_1 from "./../../images/project_bg_1.jpg"
+
 export default function Projects() {
     return (
         <div className="projects-container section-wrapper bg-grey">
@@ -8,25 +10,45 @@ export default function Projects() {
             <h3 className="sub-sub-title">
                 A selection of some of my personal projects. All source code can be found on <a href="https://github.com/DankersW" target="_blank" rel="noopener noreferrer">Github</a>
             </h3>
+
             <div className="project-grid">
-                <div className="project-tile bg-home-automation">
-                    <div className="project-info">
-                        <h3 className="project-title">Home automation</h3>
-                        <p>Some long text here</p>
-                        <p>Tools</p>
-                        <p>link</p>
-                    </div>
-                    <div className="project-bg">
-                        pic
-                    </div>  
+
+                <div className="grid-item bg-abstact-1">
+                    <h3 className="grid-item-title">Home automation</h3>
+                    <div className="grid-item-overlay">
+			            <div className="overlay-description">
+                            <h3 className="overlay-title">Home automation</h3>
+				            <p>A little shiny description</p>
+                            <p>Tools: Python, Sketch-up, docker</p>
+                            <a href={"https://www.google.com"} target="_blank" rel="noopener noreferrer">
+                                Link
+                            </a>
+                            
+			            </div>
+		            </div>
                 </div>
 
-                <div className="project-tile">Portfolio website</div>
-                <div className="project-tile">Snow plow robot</div>
-                <div className="project-tile">IEEE publicaion</div>
-                <div className="project-tile">AI poker player</div>
-                <div className="project-tile">DIY bed with storage</div>
-                <div className="project-tile">Drone</div>
+                <div className="grid-item bg-abstact-2">
+                    <h3 className="grid-item-title">DIY bed with storage</h3>
+                    <div className="grid-item-overlay">
+			            <div className="overlay-description">
+                            <h3 className="overlay-title">Home automation</h3>
+				            <p>A little shiny description</p>
+			            </div>
+		            </div>
+                </div>
+
+                
+
+
+
+                <div className="grid-item bg-abstact-1">Home automation</div>
+                <div className="grid-item bg-abstact-1">Portfolio website</div>
+                <div className="grid-item bg-abstact-2">Snow plow robot</div>
+                <div className="grid-item bg-abstact-3">IEEE publicaion</div>
+                <div className="grid-item bg-abstact-4">AI poker player</div>
+                <div className="grid-item bg-abstact-5">DIY bed with storage</div>
+                <div className="grid-item bg-abstact-6">Drone</div>
 
             </div>
 
