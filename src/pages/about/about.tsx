ef7455514cf568bf1bc8af84dd1aa81e6fd68905
DankersW <wouter.dankers@skynet.be>
2021-04-04 23:04:44 +0200

    flexbox for big displays

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./about.css"
+import wouterSnowPic from "./../../images/wouter.jpg"
 
 
 export default function About() {
     return (
         <div className="about-container bg-dark-grey section-wrapper">
             <h1 className="section-title">About</h1>
+            <div className="about-flex-wrapper">
+                <div className="about-text">
+                Born and raised in the country side of Flanders Belgium. <br/>
+                I Studied a bachelors degree in ICT from the Thomas More Hogeschool Geel Belgium and received a Master of Science degree in Embedded and Inteligent systems from Halmstad University.<br/>
+                I take great pride in writing well fuctioning code that scalles and ages well. <br/>
+                When I'm not working or writen software for my pet projects, I try to be outside as much as possible. I'm an active outdours person who enjoys many sports and activies, Skiing, ice-hockey, rock-climbing, surfing, etc. just to name a few.<br/>
+                Currently Wouter is living together with his girlfriend Mathilda in a cosy apartment in the center of Göteborg.<br/>
+                </div>
+                <div className="pic">
+                    <img src={wouterSnowPic} alt="Wouter"/>
+                </div>
+            </div>
+
             
         </div>
     );
