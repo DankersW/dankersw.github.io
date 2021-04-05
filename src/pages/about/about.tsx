68480f9311354bb153cf3d6c4fa05eecc8bbffd2
DankersW <wouter.dankers@skynet.be>
2021-04-05 12:46:39 +0200

    Allign text correctly

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -9,7 +9,7 @@ export default function About() {
             <h1 className="section-title">About</h1>
             <div className="about-wrapper">
                 <div className="about-text">
-                    <p>Born and raised in the country side of Flanders Belgium.</p>
+                    <p>Born and raised in a small village on the country side of Flanders Belgium called Lichtaart.</p>
                     <p>I Studied a bachelors degree in ICT from the Thomas More Hogeschool Geel Belgium and received a Master of Science degree in Embedded and Inteligent systems from Halmstad University.</p>
                     <p>I take great pride in writing well fuctioning code that scalles and ages well.</p>
                     <p>When I'm not working or writen software for my pet projects, I try to be outside as much as possible. I'm an active outdours person who enjoys many sports and activies, Skiing, ice-hockey, rock-climbing, surfing, etc. just to name a few.</p>
