528efe01a328c8a9e41463a2533306c3ebcdfe9b
DankersW <wouter.dankers@skynet.be>
2021-04-05 13:27:05 +0200

    cleanup about page

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -31,24 +31,3 @@ export default function About() {
         </div>
     );
 }
-
-/*
-import wouterSnowPic from "./../../images/wouter.jpg"
-<div className="about-flex-container">
-                <div className="about-info">
-                    Born and raised in the country side of Flanders Belgium. He Studied a bachelors degree in ICT from the Thomas More Hogeschool Geel Belgium and received a Master of Science degree in Embedded and Inteligent systems from Halmstad University.
-
-                    Wouter takes great pride in writing well fuctioning code that scalles and ages well. 
-
-                    When Wouter is not working or writen software for his home projects, he tries to be outside as much as possible. Wouter an active outdours person who enjoys many sports and activies, Skiing, ice-hockey, rock-climbing, surfing, etc. just to name a few.
-                    
-
-                    Currently Wouter is living together with his girlfriend Mathilda in a cosy apartment in the center of Göteborg.
-
-
-                </div> 
-                <div className="about-pic">
-                    <img src={wouterSnowPic} alt="BigCo Inc. logo"/>
-                </div>
-            </div>
-*/
