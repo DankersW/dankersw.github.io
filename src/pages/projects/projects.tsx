43d8c12d79da0e58241c7080e08015a2ef7fc975
DankersW <wouter.dankers@skynet.be>
2022-04-01 17:22:34 +0200

    added a note for cv

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -7,8 +7,12 @@ export default function Projects() {
         <div className="projects-container section-wrapper bg-dark-grey">
             <h1 className="section-title">Projects</h1>
             <h3 className="sub-sub-title">
-                A selection of some of my personal projects. All source code can be found on <a href="https://github.com/DankersW" target="_blank" rel="noopener noreferrer">Github</a>
+                A selection of some of my pet projects. All source code is publically availible on <a href="https://github.com/DankersW" target="_blank" rel="noopener noreferrer">Github</a>.
+
             </h3>
+            <h4 className="sub-sub-sub-title"> 
+                For my work projects please consult my <a href="https://github.com/DankersW" target="_blank" rel="noopener noreferrer">CV</a> or <a href="https://github.com/DankersW" target="_blank" rel="noopener noreferrer">portfolio.</a>.
+            </h4>
             <div className="project-grid">
                 {projectData.map((item, index) => {
                     return (
