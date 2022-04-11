0153ce1c8b56992ce37cc02f832e5891244b6c31
DankersW <wouter.dankers@skynet.be>
2022-04-11 23:28:50 +0200

    cleanup

--- src/pages/projects/projectData.tsx
+++ src/pages/projects/projectData.tsx
@@ -6,25 +6,25 @@ export const projectData = [
         link: "https://github.com/DankersW/home-automation"
     },
     {
-        bgPic: "bg-abstact-2",
+        bgPic: "bg-wave",
         title: "Portfolio website",
         description: "The website you're looking at. My portfolio website is writen in Typescript using React",
         link: "https://github.com/DankersW/dankersw.github.io"
     },
     {
-        bgPic: "bg-abstact-3",
+        bgPic: "bg-drone",
         title: "DIY drone",
         description: "Powerfull lifting drone made from skretch. The drone got stolen before I managed to finish it",
         link: "https://i.scdn.co/image/ab67616d0000b2732f9456e33057335ebec5b781"
     },
     {
-        bgPic: "bg-abstact-4",
+        bgPic: "bg-robot",
         title: "Snowplow robot",
         description: "Autonomous robot snowplow fleet for the course Design of Embedded and Intelligent Systems",
         link: "https://github.com/DankersW/DEIS_robot"
     },
     {
-        bgPic: "bg-abstact-1",
+        bgPic: "bg-research",
         title: "IEEE publication",
         description: "Published several paperrs from the results of my master thesis in IEEE Transactions on Vehicular Technology",
         link: "https://ieeexplore.ieee.org/abstract/document/8771197"
@@ -36,7 +36,7 @@ export const projectData = [
         link: "https://www.youtube.com/watch?v=l3N-GKR2fxE&ab_channel=Saent"
     },
     {
-        bgPic: "bg-abstact-7",
+        bgPic: "bg-woodworking",
         title: "Woodworking",
         description: "Projects ranging from a storage bed to build-in closets. I draw my projects before hand in 3D using Sketch-up",
         link: ""
